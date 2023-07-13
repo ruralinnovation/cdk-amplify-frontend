82bd0d034b467478677a20662da168af80a087fd
John Hall <john.hall@ruralinnovation.us>
2023-07-12 21:09:12 -0400

    publish to amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,15 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "hosting": {
+    "ElasticContainer": {
+      "CloudfrontDistributionAliasDomainName": "string",
+      "CloudfrontDistributionCnameDomainName": "string",
+      "ClusterName": "string",
+      "ContainerNames": "string",
+      "LoadBalancerAliasDomainName": "string",
+      "LoadBalancerCnameDomainName": "string",
+      "PipelineName": "string",
+      "PipelineUrl": "string",
+      "ServiceName": "string"
+    }
+  }
+}
